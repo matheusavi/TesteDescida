ec2c3e2272c36e23f6c40f9e817033144a107727
Matheus Avi <matheusavi.47@gmail.com>
2019-12-04 20:15:40 -0300

    commit inicial

---
+++ Assets/Scripts/Chao.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+    void Start()
+    {
+        //gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/husky.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class husky : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.AddForce(new Vector2(3, 0));
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.collider.gameObject.tag == "Ground")
+        {
+            rigidBody.constraints = RigidbodyConstraints2D.None;
+
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, col.contacts[0].normal);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+    }
+}
