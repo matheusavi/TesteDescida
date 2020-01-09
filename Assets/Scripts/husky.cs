de1096a7aa775d490a38b71888012a80ca295fbd
Matheus Avi <matheusavi.47@gmail.com>
2020-01-08 22:44:27 -0300

    manual rotation added to the player

--- Assets/Scripts/husky.cs
+++ Assets/Scripts/husky.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Husky : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
-    public int adjustRotationSpeed;
+    public int automaticRotationSpeed;
+    public int manualRotationSpeed;
+    public Text Text;
+    private bool rotating = false;
+    private bool onGround = false;
+
     void Start()
     {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
@@ -11,11 +17,43 @@ public class Husky : MonoBehaviour
 
     void Update()
     {
-        //rigidBody.AddForce(new Vector2(3, 0));
+        AutomaticGroundAligment();
 
+        RotateControl();
+    }
+
+    private void RotateControl()
+    {
+
+#if UNITY_EDITOR
+        //Rotate with mouse
+        if (Input.GetMouseButton(0))
+        {
+            Rotate();
+        }
+        else
+        {
+            rotating = false;
+        }
+#else
+        //Rotate with touch
+        if (Input.touchCount > 0)
+        {
+            Rotate();
+        }
+        else
+        {
+            rotating = false;
+        }
+#endif
+
+    }
+
+    private void AutomaticGroundAligment()
+    {
         var hit = Physics2D.Raycast(transform.position, -transform.up, 20, LayerMask.GetMask("Ground"));
 
-        if (hit && hit.distance > 1.0f)
+        if (!rotating && hit && hit.distance > 1.0f)
         {
             if (hit.distance > 1.5f)
                 rigidBody.freezeRotation = true;
@@ -24,12 +62,33 @@ public class Husky : MonoBehaviour
 
             Debug.DrawLine(transform.position, hit.point, Color.green);
             var rot = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * adjustRotationSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * automaticRotationSpeed);
         }
         else
         {
             rigidBody.freezeRotation = false;
         }
+    }
+
+    private void Rotate()
+    {
+        if (!onGround)
+        {
+            rotating = true;
+            transform.Rotate(new Vector3(0, 0, manualRotationSpeed));
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            onGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            onGround = false;
     }
 }
 
